87fa5049f65d21a2a3f8c8540eadd4b0b24a17f1
s-knk <625.somq2525@gmail.com>
2026-07-04 21:52:01 +0900

    App Store提出用バージョンを1.0.1に更新

--- app.config.ts
+++ app.config.ts
@@ -7,7 +7,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: '今すぐタイマー',
   slug: 'widget-timer',
-  version: '1.0.0',
+  version: '1.0.1',
   orientation: 'portrait',
   icon: './assets/icon.png',
   scheme: 'imasugutimer',
